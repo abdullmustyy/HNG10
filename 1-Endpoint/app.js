07559bafb84994da740f496945ca20c3b1927483
abdullmustyy <abdulmusty03@outlook.com>
2023-09-10 16:51:15 +0100

    Chore: Change port to 3001

--- 1-Endpoint/app.js
+++ 1-Endpoint/app.js
@@ -42,5 +42,5 @@ app.get("/api", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:3000`);
+  console.log(`Server is running on http://localhost:3001`);
 });
