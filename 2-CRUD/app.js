7630763fe02aa1e9d37130a895640ecb8272d25d
abdullmustyy <abdulmusty03@outlook.com>
2023-09-11 08:12:44 +0100

    Chore: Add model validation and update error handling

--- 2-CRUD/app.js
+++ 2-CRUD/app.js
@@ -8,7 +8,9 @@ import "dotenv/config";
 const app = express();
 
 // Create a person model
-const personSchema = new Schema({ name: String });
+const personSchema = new Schema({
+  name: { type: String, required: [true, "You need to provide a name"] },
+});
 const Person = model("Person", personSchema);
 
 // Body parser
@@ -25,17 +27,26 @@ app.post("/api", async (req, res) => {
   const { name } = req.body;
 
   try {
-    const personExist = await Person.find({ name: name });
+    if (typeof name !== "number") {
+      return res.status(400).json({ message: "Name must be a string" });
+    }
 
-    if (personExist.length) {
-      return res.status(409).json({ message: `${name} already exists` });
+    const personExists = await Person.find({ name: name });
+
+    if (personExists.length) {
+      return res
+        .status(409)
+        .json({ message: `${name} already exists`, personExists });
     }
 
     const person = await Person.create({ name });
 
     res.status(201).json({ message: `CREATED ${name}`, person });
   } catch (error) {
-    res.status(500).json({ message: `Error creating ${name}` });
+    res.status(500).json({
+      message: `Error creating ${name || "person"}`,
+      error: `${error.errors.name.path}: ${error.errors.name.message}`,
+    });
   }
 });
 
@@ -55,7 +66,9 @@ app.get("/api/:id", async (req, res) => {
 
     res.status(200).json({ message: `READ ${personName}`, person });
   } catch (error) {
-    res.status(500).json({ message: `Error finding ${personName}` });
+    res
+      .status(500)
+      .json({ message: `Error finding ${personName || "person"}` });
   }
 });
 
@@ -81,7 +94,9 @@ app.patch("/api/:id", async (req, res) => {
       personUpdate,
     });
   } catch (error) {
-    res.status(500).json({ message: `Error updating ${personName.name}` });
+    res
+      .status(500)
+      .json({ message: `Error updating ${personName.name || "person"}` });
   }
 });
 
@@ -101,7 +116,9 @@ app.delete("/api/:id", async (req, res) => {
 
     res.status(200).json({ message: `DELETED ${personName.name}` });
   } catch (error) {
-    res.status(500).json({ message: `Error deleting ${personName.name}` });
+    res
+      .status(500)
+      .json({ message: `Error deleting ${personName.name || "person"}` });
   }
 });
 
